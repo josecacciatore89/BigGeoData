8d2ad6ae331a26fffec06f91643c6f5269b7f9bf
josecacciatore89 <60232884+josecacciatore89@users.noreply.github.com>
2025-04-28 17:12:15 -0300

    Add files via upload

---
+++ Config_ambiente_1_0.bat
@@ -0,0 +1,8 @@
+# Criar o ambiente (só a primeira vez)
+conda create -n nome_do_ambiente python=3.11
+
+# Ativar o ambiente
+conda activate nome_do_ambiente
+
+# Depois, abrir o Jupyter Notebook
+jupyter notebook
